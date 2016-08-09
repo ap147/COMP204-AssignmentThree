9fddb854e3b8c943a328f32a6ed485566b07a525
Amarjot Parmar <Amarjot@Amarjot8-2.local>
2016-08-10 01:03:06 +1200

    Started Task 3 (Project)

--- JavaFX_Project/src/org/waikato/comp204/Project.java
+++ JavaFX_Project/src/org/waikato/comp204/Project.java
@@ -1,3 +1,27 @@
 package org.waikato.comp204;
 
-// Your Project JavaFX code goes here
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+
+public class Project extends Application
+{
+
+}
+
