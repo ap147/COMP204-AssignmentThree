889acac29cc1d74914467f767c21cb20e3206e33
Amarjot Parmar <Amarjot@Amarjot8-2.local>
2016-08-10 17:02:59 +1200

    Started to implement auto correct in text fields where only numiric fields are allowed

--- JavaFX_Project/src/org/waikato/comp204/Project.java
+++ JavaFX_Project/src/org/waikato/comp204/Project.java
@@ -1,5 +1,6 @@
 package org.waikato.comp204;
-
+//https://www.youtube.com/watch?v=QGGE0WsUslc
+//http://stackoverflow.com/questions/32866937/how-to-check-if-textfield-is-empty
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -22,16 +23,28 @@ import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 
-public class Project extends Application
-{
+public class Project extends Application {
     private static GridPane grid = new GridPane();
-    public static void main(String []args)
-    {
+
+    private static String itemA;
+    private static String itemB;
+    private static String itemC;
+
+    private static int amountA;
+    private static int amountB;
+    private static int amountC;
+
+    private static double priceA;
+    private static double priceB;
+    private static double priceC;
+
+
+    public static void main(String[] args) {
         launch(args);
     }
+
     @Override
-    public void start(Stage primaryStage)
-    {
+    public void start(Stage primaryStage) {
         primaryStage.setTitle("JavaFX Project");
         setupGrid();
         setupInputTextFields();
@@ -51,55 +64,65 @@ public class Project extends Application
 
     }
 
-    public static void setupGrid()
-    {
+    public static void setupGrid() {
         grid.setHgap(10);
         grid.setVgap(10);
-        grid.setPadding(new Insets(0,20,0,20));
+        grid.setPadding(new Insets(0, 20, 0, 20));
         //grid.setGridLinesVisible(true);
 
     }
 
-    public static void setupInputTextFields()
-    {
+    public static void setupInputTextFields() {
 
         // ------------------    INPUT    ------------------
         // ----------- ITEMS
         TextField inputItemA = new TextField();
         grid.add(inputItemA, 1, 4);
         inputItemA.setPrefWidth(200);
-        inputItemA.setOnKeyReleased(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                System.out.println(inputItemA.getText());
-            }
-        });
+        inputItemA.setOnKeyReleased(event -> itemA = inputItemA.getText());
 
         TextField inputItemB = new TextField();
         grid.add(inputItemB, 1, 5);
         inputItemB.setPrefWidth(200);
+        inputItemB.setOnKeyReleased(event -> itemB = inputItemB.getText());
 
         TextField inputItemC = new TextField();
         grid.add(inputItemC, 1, 6);
         inputItemC.setPrefWidth(200);
+        inputItemC.setOnKeyReleased(event -> itemC = inputItemC.getText());
 
         // ---------- AMOUNT
         TextField inputAmountA = new TextField();
         grid.add(inputAmountA, 2, 4);
         inputAmountA.setPrefWidth(50);
+        inputAmountA.setOnKeyReleased(event -> amountA = Integer.parseInt(inputAmountA.getText()));
 
         TextField inputAmountB = new TextField();
         grid.add(inputAmountB, 2, 5);
         inputAmountB.setPrefWidth(50);
+        inputAmountB.setOnKeyReleased(event -> amountB = Integer.parseInt(inputAmountB.getText()));
 
         TextField inputAmountC = new TextField();
         grid.add(inputAmountC, 2, 6);
         inputAmountC.setPrefWidth(50);
+        inputAmountC.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (CheckIfNum(inputAmountC.getText())) {
+                    amountC = Integer.parseInt(inputAmountC.getText());
+                } else {
+                    if (!inputAmountC.getText().trim().isEmpty()) {
+                        inputAmountC.setText(amountC + "");
+                    }
+                }
+            }
+        });
 
         // ---------- PRICE
         TextField inputPriceA = new TextField();
-        grid.add(inputPriceA,3, 4);
+        grid.add(inputPriceA, 3, 4);
         inputPriceA.setPrefWidth(75);
+        //inputPriceA.setOnKeyReleased(event -> priceA = double.parseInt(priceA.getText()));
 
         TextField inputPriceB = new TextField();
         grid.add(inputPriceB, 3, 5);
@@ -110,7 +133,23 @@ public class Project extends Application
         inputPriceC.setPrefWidth(75);
     }
 
-    public static void Calculate()
+    private static void CheckTextBox(TextField x)
+    {
+
+    }
+    private static boolean CheckIfNum(String num)
+    {
+        try{
+            int x = Integer.parseInt(num);
+            return true;
+        }
+        catch(Exception e)
+        {
+        }
+        return false;
+    }
+
+    private static void Calculate()
     {
 
     }
