5f16dd63743a7a0a5bc301006d1f70a67c0ceb10
Amarjot Parmar <Amarjot@Amarjot8-2.local>
2016-08-10 15:34:27 +1200

    All Input TextFields Added / Positioned

--- JavaFX_Project/src/org/waikato/comp204/Project.java
+++ JavaFX_Project/src/org/waikato/comp204/Project.java
@@ -17,6 +17,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 
@@ -35,22 +36,60 @@ public class Project extends Application
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(0,20,0,20));
+        //grid.setGridLinesVisible(true);
 
 
-        //Setting Titles
-        Text itemTitle = new Text("Item");
-        itemTitle.setFont(Font.font("Arial",FontWeight.BOLD, 16));
-        grid.add(itemTitle, 1, 1);
+        // ------------------    INPUT    ------------------
+        // ----------- ITEMS
+        TextField inputItemA = new TextField();
+        grid.add(inputItemA, 1, 4);
+        inputItemA.setPrefWidth(200);
 
-        Text categoryTitle = new Text("Amount");
-        categoryTitle.setFont(Font.font("Arial", FontWeight.BOLD, 16));
-        grid.add(categoryTitle, 10, 1);
+        TextField inputItemB = new TextField();
+        grid.add(inputItemB, 1, 5);
+        inputItemB.setPrefWidth(200);
 
-        Scene scene = new Scene(grid, 600, 300);
+        TextField inputItemC = new TextField();
+        grid.add(inputItemC, 1, 6);
+        inputItemC.setPrefWidth(200);
+
+        // ---------- AMOUNT
+        TextField inputAmountA = new TextField();
+        grid.add(inputAmountA, 2, 4);
+        inputAmountA.setPrefWidth(50);
+
+        TextField inputAmountB = new TextField();
+        grid.add(inputAmountB, 2, 5);
+        inputAmountB.setPrefWidth(50);
+
+        TextField inputAmountC = new TextField();
+        grid.add(inputAmountC, 2, 6);
+        inputAmountC.setPrefWidth(50);
+
+        // ---------- PRICE
+        TextField inputPriceA = new TextField();
+        grid.add(inputPriceA,3, 4);
+        inputPriceA.setPrefWidth(75);
+
+        TextField inputPriceB = new TextField();
+        grid.add(inputPriceB, 3, 5);
+        inputPriceB.setPrefWidth(75);
+
+        TextField inputPriceC = new TextField();
+        grid.add(inputPriceC, 3, 6);
+        inputPriceC.setPrefWidth(75);
+
+        Line line = new Line();
+
+        line.setStartX(100f);
+        line.setStartY(100f);
+        line.setEndX(100f);
+        line.setEndY(300f);
+
+        Scene scene = new Scene(grid, 700, 250);
         primaryStage.setScene(scene);
 
         primaryStage.show();
-
     }
 
 
